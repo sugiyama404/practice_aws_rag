dfc65220f3a48c081b6aa28b42c6f60a8a9758ad
sugiyama404 <mmingriha@gmail.com>
2024-05-18 11:53:47 +0900

    delete console log

--- webserver/app/app/api/route.ts
+++ webserver/app/app/api/route.ts
@@ -4,9 +4,7 @@ import { NextResponse, NextRequest } from 'next/server'
 const target: string = process.env.API_ENDPOINT;
 
 export async function POST(request: NextRequest) {
-    console.log("call me");
     const query = await request.json();
-    console.log(query.user_query);
     const res = await fetch(target, {
         method: "POST",
         headers: {
